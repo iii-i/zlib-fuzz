83e53151c260d3b75185e664e840b0b6dd4f5362
Ilya Leoshkevich <iii@linux.ibm.com>
2021-10-19 04:12:13 +0200

    Get rid of PostProcessorRegistration

--- fuzz_target.cpp
+++ fuzz_target.cpp
@@ -502,27 +502,31 @@ static void RunInflate(const Plan &Plan, const uint8_t *Compressed,
   }
   z_stream Strm;
   memset(&Strm, 0, sizeof(Strm));
-  int Err = inflateInit2(&Strm, Plan.window_bits());
+  int WindowBits = Plan.window_bits();
+  if (WindowBits == WB_DEFAULT)
+    WindowBits = WB_ZLIB;
+  int Err = inflateInit2(&Strm, WindowBits);
   if (Debug)
-    fprintf(stderr, "assert(inflateInit2(&Strm, %i) == %s);\n",
-            Plan.window_bits(), ErrStr(Err));
+    fprintf(stderr, "assert(inflateInit2(&Strm, %i) == %s);\n", WindowBits,
+            ErrStr(Err));
   assert(Err == Z_OK);
-  if (Plan.dict().size() > 0 && Plan.window_bits() == WB_RAW) {
+  if (Plan.dict().size() > 0 && WindowBits == WB_RAW) {
     Err = InflateSetDictionary(&Strm, (const Bytef *)Plan.dict().c_str(),
                                Plan.dict().size());
     assert(Err == Z_OK);
   }
+  size_t TailSize = Plan.tail_size() & 0xff;
   std::unique_ptr<uint8_t[]> Uncompressed(
-      new uint8_t[Plan.data().size() + Plan.tail_size()]);
+      new uint8_t[Plan.data().size() + TailSize]);
   Strm.next_in = Compressed;
   Strm.avail_in = ActualCompressedSize;
   Strm.next_out = Uncompressed.get();
-  Strm.avail_out = Plan.data().size() + Plan.tail_size();
+  Strm.avail_out = Plan.data().size() + TailSize;
   for (int i = 0; i < Plan.inflate_ops_size(); i++) {
     Err = VisitOp(Plan.inflate_ops(i), OpRunner(&Strm, Check));
     if (Err == Z_NEED_DICT) {
       if (Check)
-        assert(Plan.dict().size() > 0 && Plan.window_bits() == WB_ZLIB);
+        assert(Plan.dict().size() > 0 && WindowBits == WB_ZLIB);
       Err = InflateSetDictionary(&Strm, (const Bytef *)Plan.dict().c_str(),
                                  Plan.dict().size());
       if (Check)
@@ -533,7 +537,7 @@ static void RunInflate(const Plan &Plan, const uint8_t *Compressed,
     Err = Inflate(&Strm, Z_NO_FLUSH);
     if (Err == Z_NEED_DICT) {
       if (Check)
-        assert(Plan.dict().size() > 0 && Plan.window_bits() == WB_ZLIB);
+        assert(Plan.dict().size() > 0 && WindowBits == WB_ZLIB);
       Err = InflateSetDictionary(&Strm, (const Bytef *)Plan.dict().c_str(),
                                  Plan.dict().size());
       if (Check)
@@ -544,7 +548,7 @@ static void RunInflate(const Plan &Plan, const uint8_t *Compressed,
   if (Check) {
     assert(Err == Z_STREAM_END);
     assert(Strm.avail_in == 0);
-    assert(Strm.avail_out == (uInt)Plan.tail_size());
+    assert(Strm.avail_out == TailSize);
     assert(memcmp(Uncompressed.get(), Plan.data().c_str(),
                   Plan.data().size()) == 0);
   }
@@ -567,15 +571,21 @@ static void RunPlan(Plan &Plan) {
   std::unique_ptr<uint8_t[]> Compressed(new uint8_t[CompressedSize]);
   z_stream Strm;
   memset(&Strm, 0, sizeof(Strm));
-  int Err = deflateInit2(&Strm, Plan.level(), Z_DEFLATED, Plan.window_bits(),
-                         Plan.mem_level(), Plan.strategy());
+  int WindowBits = Plan.window_bits();
+  if (WindowBits == WB_DEFAULT)
+    WindowBits = WB_ZLIB;
+  int MemLevel = Plan.mem_level();
+  if (MemLevel == MEM_LEVEL_DEFAULT)
+    MemLevel = MEM_LEVEL8;
+  int Err = deflateInit2(&Strm, Plan.level(), Z_DEFLATED, WindowBits, MemLevel,
+                         Plan.strategy());
   if (Debug)
     fprintf(stderr,
             "assert(deflateInit2(&Strm, %i, Z_DEFLATED, %i, %i, %s) == %s);\n",
-            Plan.level(), Plan.window_bits(), Plan.mem_level(),
-            StrategyStr(Plan.strategy()), ErrStr(Err));
+            Plan.level(), WindowBits, MemLevel, StrategyStr(Plan.strategy()),
+            ErrStr(Err));
   assert(Err == Z_OK);
-  if (Plan.dict().size() > 0) {
+  if (Plan.dict().size() > 0 && WindowBits != WB_GZIP) {
     Err = DeflateSetDictionary(&Strm, (const Bytef *)Plan.dict().c_str(),
                                Plan.dict().size());
     assert(Err == Z_OK);
@@ -644,17 +654,6 @@ static void RunPlan(Plan &Plan) {
 }
 
 #ifdef USE_LIBPROTOBUF_MUTATOR
-static protobuf_mutator::libfuzzer::PostProcessorRegistration<Plan> reg = {
-    [](Plan *Plan, unsigned int /* Seed */) {
-      if (Plan->window_bits() == WB_DEFAULT)
-        Plan->set_window_bits(WB_ZLIB);
-      if (Plan->mem_level() == MEM_LEVEL_DEFAULT)
-        Plan->set_mem_level(MEM_LEVEL8);
-      if (Plan->window_bits() == WB_GZIP)
-        Plan->clear_dict();
-      Plan->set_tail_size(Plan->tail_size() & 0xff);
-    }};
-
 DEFINE_PROTO_FUZZER(const Plan &Plan) {
   class Plan PlanCopy = Plan;
   RunPlan(PlanCopy);
